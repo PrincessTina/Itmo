75dca3d2c13702aaaf48e681c6a2dc66dffdab43
Gleb <gleb>
2017-05-12 13:54:11 +0300

    Delete Serializable

--- programming/lab5/src/com/company/Shorty.java
+++ programming/lab5/src/com/company/Shorty.java
@@ -4,7 +4,7 @@ import java.io.*;
 import java.util.ArrayList;
 import java.util.Comparator;
 
-public class Shorty implements Serializable, Comparator<Shorty> {
+public class Shorty implements Comparator<Shorty> {
   private String name;
   private int age;
   private double height;
