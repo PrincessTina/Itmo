fb34231b5500dde1d775484937c118e93aab6293
Gleb <gleb>
2017-05-15 14:01:32 +0300

    Nachalo konca!

--- lab62/src/com/company/Main.java
+++ lab62/src/com/company/Main.java
@@ -10,13 +10,21 @@ import org.eclipse.swt.widgets.Shell;
 public class Main {
 
     public static void main(String[] args) {
-      Display display = new Display();
-      Shell sh = new Shell(display);
-      sh.setLayout(new FillLayout());
-      Label l = new Label(sh, SWT.BORDER);
-      l.setText("Hello!");
-      sh.open();
+        Display display = new Display();
 
-      display.dispose();
+        Shell shell = new Shell(display);
+
+        // the layout manager handle the layout
+        // of the widgets in the container
+        shell.setLayout(new FillLayout());
+
+        //TODO add some widgets to the Shell
+        shell.open();
+        while (!shell.isDisposed()) {
+            if (!display.readAndDispatch()) {
+                display.sleep();
+            }
+        }
+        display.dispose();
     }
 }
