174f460ca72740a82c86581640df2fce651c130e
PrincessTina <kristina.orl-va@bk.ru>
2017-09-16 13:07:30 +0300

    Almost ready. Some css left

--- pip/lab1/main.php
+++ pip/lab1/main.php
@@ -2,12 +2,23 @@
 
 global $_POST;
 
-//объявляем POST переменные
+$start = microtime(true);
+
 $x = $_POST['x'];
 $y = $_POST['y'];
 $r = $_POST['r'];
-$arr = array('x' => $x, 'y' => $y, 'r' => $r);
+
+$result;
+
+if ((($x > 0) && ($x <= $r) && ($y >= -$r / 2) && ($y <= 0)) || (($x == 0) && ($y >= -$r / 2) && ($y <= $r)) ||
+    (($x < 0) && ($y > 0) && ($y <= $r) && ($x >= -$r / 2) && ($y <= 2 * $x + $r)) || (($x < 0) && ($y < 0) &&
+        (($x ^ 2 + $y ^ 2) < $r ^ 2))) {
+    $result = "The point passed the test";
+} else {
+    $result = "The point didn't passed  the test";
+}
+$finish = microtime(true);
+$delta = $finish - $start;
+$arr = array('result' => $result, 'current_time' => date("H:i:s d/m/Y"), "work_time" => $delta);
 echo json_encode($arr);
-
-
 
--- pip/lab1/validate.js
+++ pip/lab1/validate.js
@@ -1,13 +1,15 @@
+
 function check_field() {
     var x = document.getElementById('x');
     var p = parseFloat(x.value);
 
     if ((!/^-?\d+.?\d*$/.test(x.value.trim())) || (x.value.trim() === "")) {
-        document.getElementById('x_span').innerHTML = "Неверный формат числа";
+        document.getElementById('x_span').innerHTML = "Invalid format";
+        document.getElementById('button').disabled = true;
     } else if ((p <= -3) || (p >= 3)) {
-        document.getElementById('x_span').innerHTML = "Число выходит за ОДБ";
+        document.getElementById('x_span').innerHTML = "The number is out of ODB";
+        document.getElementById('button').disabled = true;
     } else {
-        console.log(p);
         document.getElementById('tick').innerHTML = "*";
     }
 }
@@ -15,6 +17,57 @@ function check_field() {
 function returnField() {
     document.getElementById('x_span').innerHTML = "";
     document.getElementById('tick').innerHTML = "";
+    document.getElementById('button').disabled = false;
+}
+
+function nextField(id) {
+    var tab;
+
+    if (id === 1) {
+        tab = "tab2";
+    } else {
+        tab = "tab3";
+    }
+    document.getElementById(tab).checked = true;
+}
+
+function exchangeParameters() {
+    var x = document.getElementById('x').value;
+    var y = getY();
+    var r = getR();
+    var arr;
+    var xmlhttp = new XMLHttpRequest();
+
+    xmlhttp.onreadystatechange = function() {
+        if (xmlhttp.readyState === XMLHttpRequest.DONE ) {
+            if (xmlhttp.status === 200) {
+                arr = JSON.parse(xmlhttp.responseText);
+
+                document.getElementById("x_table").innerHTML = x;
+                document.getElementById("y_table").innerHTML = y;
+                document.getElementById("r_table").innerHTML = r;
+                document.getElementById("result").innerHTML = arr.result;
+                document.getElementById("current_time").innerHTML = arr.current_time;
+                document.getElementById("work_time").innerHTML = arr.work_time;
+                document.getElementById("tab4").checked = true;
+            }
+            else if (xmlhttp.status === 400) {
+                alert('There was an error 400');
+            }
+            else {
+                alert('something else other than 200 was returned');
+            }
+        }
+    };
+
+    xmlhttp.open("POST", "main.php", true);
+    xmlhttp.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+    xmlhttp.send("x=" + x + "&y=" + y + "&r=" + r);
+}
+
+function getY() {
+    var e = document.getElementById("y");
+    return e.options[e.selectedIndex].text;
 }
 
 function getR() {
@@ -29,33 +82,6 @@ function getR() {
     return R;
 }
 
-function getY() {
-    var e = document.getElementById("y");
-    return e.options[e.selectedIndex].text;
-}
-
-function exchangeParameters() {
-    var x = document.getElementById('x');
-    var xmlhttp = new XMLHttpRequest();
-
-    xmlhttp.onreadystatechange = function() {
-        if (xmlhttp.readyState === XMLHttpRequest.DONE ) {
-            if (xmlhttp.status === 200) {
-                document.getElementById("x_table").innerHTML = xmlhttp.responseText;
-            }
-            else if (xmlhttp.status === 400) {
-                alert('There was an error 400');
-            }
-            else {
-                alert('something else other than 200 was returned');
-            }
-        }
-    };
-
-    xmlhttp.open("POST", "main.php", true);
-    xmlhttp.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
-    xmlhttp.send("x=" + x.value + "&y=" + getY() + "&r=" + getR());
-}
 
 
 
