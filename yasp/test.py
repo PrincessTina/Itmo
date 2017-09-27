7783555cf5098d29e90985d2dcf543b13b2a0fae
PrincessTina <kristina.orl-va@bk.ru>
2017-09-28 01:55:42 +0300

    lab1 was done

--- yasp/test.py
+++ yasp/test.py
@@ -177,26 +177,6 @@ tests=[ Test('string_length',
         syscall""", 
         lambda i,o,r: i == o),
 
-        Test('string_copy',
-            lambda v: """section .data
-        arg1: db '""" + v + """', 0
-        arg2: times """ + str(len(v) + 1) +  """ db  66
-        section .text
-        %include "lib.inc"
-        global _start 
-        _start:
-        """ + before_call + """
-        mov rdi, arg1
-        mov rsi, arg2
-        call string_copy
-        """ + after_call + """
-        mov rdi, arg2 
-        call print_string
-        mov rax, 60
-        xor rdi, rdi
-        syscall""", 
-        lambda i,o,r: i == o),
-
         Test('print_char',
             lambda v:""" section .text
         %include "lib.inc"
