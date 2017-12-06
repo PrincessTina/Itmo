87b0430d52a0c0c337146142be6da06ac97cb52d
PrincessTina <kristina.orl-va@bk.ru>
2017-12-07 02:21:51 +0300

    pip: db left

--- pip/lab3/src/main/java/beans/PaintBean.java
+++ pip/lab3/src/main/java/beans/PaintBean.java
@@ -1,31 +1,65 @@
 package beans;
 
-import classes.Paint;
+import classes.Point;
+
 import java.util.ArrayList;
 
 import java.io.Serializable;
-import java.util.Arrays;
 
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 
-@ManagedBean(name = "paint")
+import static java.lang.Math.pow;
+
+@ManagedBean(name = "point")
 @SessionScoped
 public class PaintBean implements Serializable {
   private double x;
-  private int y;
-  private double r;
+  private double y = -3;
+  private double r = 1.0;
   private String result;
-  private static final ArrayList<Paint> paints = new ArrayList<>(Arrays.asList(
-      new Paint(1.0, -4, 3.0, "better"),
-      new Paint(2.0, 2, 3.0, "worse"),
-      new Paint(3.0, 0, 3.0, "bad"),
-      new Paint(4.0, 12, 3.0, "bad"),
-      new Paint(5.0, -9, 3.0, "good")
-  ));
+  private static final ArrayList<Point> paints = new ArrayList<>();
 
   public void add() {
-    paints.add(new Paint(this.x, this.y, this.r, this.result));
+    check();
+
+    if (result != null) {
+      paints.add(new Point(this.x, this.y, this.r, this.result));
+    }
+  }
+
+  private void check() {
+    if (checkParams(x, y, r)) {
+      if (checkPoint(x, y, r)) {
+        result = "Point is in the scope";
+      } else {
+        result = "Point is out of the scope";
+      }
+    } else {
+      result = null;
+    }
+  }
+
+  private boolean checkPoint(double x, double y, double r) {
+    if ((pow(x, 2) + pow(y, 2) <= pow(r, 2)) && (x <= 0) && (y >= 0)) {
+      return true;
+    }
+
+    if ((x <= r) && (x >= 0) && (y >= 0) && (y <= r/2)) {
+      return true;
+    }
+
+    if ((y >= -r/2 - x) && (x <= 0) && (y <= 0)) {
+      return true;
+    }
+
+    return false;
+  }
+
+  private boolean checkParams(double x, double y, double r) {
+    return !((x >= 5) || (x <= -3) || (r > 3) || (r < 1) || (y > 5) || (y < -3) ||
+        (!String.valueOf(x).matches("^-?\\d+\\.?\\d*$")) || (!String.valueOf(y).matches("^-?\\d+\\.?\\d*$")) ||
+        (!String.valueOf(r).matches("^-?\\d+\\.?\\d*$")));
   }
 
   public double getX() {
@@ -36,11 +70,11 @@ public class PaintBean implements Serializable {
     this.x = x;
   }
 
-  public int getY() {
+  public double getY() {
     return y;
   }
 
-  public void setY(int y) {
+  public void setY(double y) {
     this.y = y;
   }
 
@@ -56,11 +90,33 @@ public class PaintBean implements Serializable {
     return result;
   }
 
-  public void setResult(String  result) {
+  public void setResult(String result) {
     this.result = result;
   }
 
-  public ArrayList<Paint> getPaints() {
+  public String getString() {
+    StringBuilder answer = new StringBuilder();
+
+    for (int i = 0; i < paints.size(); i++) {
+      Point paint = paints.get(i);
+
+      if (i > 0) {
+        answer.append("; ");
+      }
+
+      answer.append(paint.getX());
+      answer.append(", ");
+      answer.append(paint.getY());
+      answer.append(", ");
+      answer.append(paint.getR());
+      answer.append(", ");
+      answer.append(paint.getResult());
+    }
+
+    return answer.toString();
+  }
+
+  public ArrayList<Point> getPaints() {
     return paints;
   }
 }
--- pip/lab3/src/main/webapp/resources/js/main.js
+++ pip/lab3/src/main/webapp/resources/js/main.js
@@ -1,7 +1,161 @@
-window.onload = function(){
-    /*window.setInterval(
-        function(){
-            var d = new Date();
-            document.getElementById("clock").innerHTML = d.toLocaleTimeString();
-        }, 1000);*/
+window.onload = function () {
+    var button = $("button[id$='button2']")[0];
+    button.disabled = true;
+    button.classList.add("ui-state-disabled");
+
+    window.setInterval(
+        function () {
+            $("input[id$='y']")[0].readOnly = true;
+        }, 400);
 };
+
+var nonCorrect = false;
+
+function update() {
+    if (!nonCorrect) {
+        createGraph();
+
+        if ($('#point_table_data').find("tr")[0].children[0].innerHTML !== "No records found.") {
+            var points = getTablePoints();
+
+            for (var i = 0; i < points.length; i++) {
+                var point = points[i];
+
+                addNewPoint(point.x, point.y, point.result);
+            }
+        }
+    }
+}
+
+function checkParams() {
+    var x = $("input[id$='x']")[0];
+    var p = parseFloat(x.value);
+
+    if ((!/^-?\d+\.?\d*$/.test(x.value.trim())) || (x.value.trim() === "")) {
+        x.style.border = "1.1px solid #c70417";
+        nonCorrect = true;
+    } else if ((p <= -3) || (p >= 5)) {
+        x.style.border = "1.1px solid #c70417";
+        nonCorrect = true;
+    } else {
+        x.style.border = "1.1px solid #EFEFEF";
+        nonCorrect = false;
+    }
+}
+
+function addNewPoint(x, y, condition) {
+    var chart = $('#container').highcharts();
+    x = parseFloat(x);
+    y = parseFloat(y);
+
+    if (condition === "Point is in the scope") {
+        chart.series[3].color = 'pink';
+    } else {
+        chart.series[3].color = 'black';
+    }
+    chart.series[3].addPoint([x, y]);
+}
+
+function createGraph() {
+    var r = parseFloat($("input[id$='r_input']")[0].value);
+    var button = $("button[id$='button2']")[0];
+    button.classList.remove("ui-state-disabled");
+    button.disabled = false;
+
+    var chart = {
+        events: {
+            'click': function (e) {
+                var x = e.xAxis[0].value,
+                    y = e.yAxis[0].value;
+
+                //series = this.series[3];
+                //series.addPoint([x, y]);
+
+                $("input[id$='x']")[0].value = x.toFixed(3);
+                checkParams();
+                $("input[id$='y']")[0].readOnly = false;
+                $("input[id$='y']")[0].value = y.toFixed(3);
+                $("button[id$='button2']")[0].click();
+                $("input[id$='y']")[0].readOnly = true;
+            }
+        }
+    };
+
+    var xAxis = {
+        maxZoom: 7,
+        gridLineWidth: 1
+    };
+
+    var yAxis = {
+        maxZoom: 7
+    };
+
+    var legend = {
+        enabled: false
+    };
+
+    var title = {
+        text: ""
+    };
+
+    var series = [
+        {
+            marker: {
+                enabled: false
+            },
+            color: '#4d1f80',
+            type: 'area',
+            data: [[0, r / 2], [r, r / 2], [r, 0]],
+            enableMouseTracking: false
+        }, {
+            marker: {
+                enabled: false
+            },
+            color: '#4d1f80',
+            type: 'area',
+            data: [[-r / 2, 0], [0, -r / 2]],
+            enableMouseTracking: false
+        }, {
+            marker: {
+                enabled: false
+            },
+            color: '#4d1f80',
+            type: 'areaspline',
+            data: [[-r, 0], [-r * Math.sqrt(3 / 4), r / 2], [-r / 2, r * Math.sqrt(3 / 4)], [0, r]],
+            enableMouseTracking: false
+        }, {
+            marker: {
+                symbol: 'diamond'
+            },
+
+            type: 'scatter'
+        }
+    ];
+
+    var json = {};
+    json.chart = chart;
+    json.title = title;
+    json.legend = legend;
+    json.xAxis = xAxis;
+    json.yAxis = yAxis;
+    json.series = series;
+
+    $('#container').highcharts(json);
+}
+
+function getTablePoints() {
+    var rows = $('#point_table_data').find("tr");
+    var points = [];
+
+
+    for (var i = 0; i < rows.length; i++) {
+        points[i] = {
+            x: parseFloat(rows[i].children[0].innerHTML),
+            y: parseFloat(rows[i].children[1].innerHTML),
+            r: parseFloat(rows[i].children[2].innerHTML),
+            result: rows[i].children[3].innerHTML
+        }
+    }
+
+    return points;
+}
