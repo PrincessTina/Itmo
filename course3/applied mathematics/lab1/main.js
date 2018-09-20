0048630fb36766c9d1bd7227742cc619e514a6a2
PrincessTina <kristina.orl-va@bk.ru>
2018-09-20 16:16:25 +0300

    done lab1

--- course3/applied mathematics/lab1/main.js
+++ course3/applied mathematics/lab1/main.js
@@ -1,7 +1,7 @@
 const fs = require("fs");
-var fileContent;
-var alphabet = {}; // объект, поля которого - символы, а их значения - объект с полями в частоту, вероятность и энтропию
-var realLength = 0;
+let fileContent;
+let alphabet = {}; // объект, поля которого - символы, а их значения - объект с полями в частоту, вероятность и энтропию
+let realLength = 0;
 
 main();
 
@@ -10,6 +10,7 @@ function main() {
     fillAdditionalInfo();
     printAlphabet();
     printEntropy();
+    printLinkedEntropy();
 }
 
 /**
@@ -21,18 +22,10 @@ function readFile(fileName) {
     fileContent = fs.readFileSync(fileName, "utf8");
     const textLength = fileContent.length;
 
-    for (var i = 0; i < textLength; i++) {
-        var symbol = fileContent[i].toLowerCase();
+    for (let i = 0; i < textLength; i++) {
+        let symbol = fileContent[i].toLowerCase();
 
         if (symbol.charCodeAt(0) >= 32 && symbol.charCodeAt(0) <= 126) {
-            
-        }
-
-        if (/[^\t\v\r\n\f]/.test(symbol)) {
-            if (!/([a-z]|[0-9]| )/.test(symbol)) {
-                symbol = ',';
-            }
-
             if (alphabet[symbol] === undefined) {
                 alphabet[symbol] = 1;
             } else {
@@ -48,10 +41,10 @@ function readFile(fileName) {
  * Добавляет в alphabet информацию о вероятности и энтропии для каждого символа
  */
 function fillAdditionalInfo() {
-    for (var key in alphabet) {
+    for (let key in alphabet) {
         alphabet[key] = {"frequency": alphabet[key], "probability": 0, "entropy": 0};
-        alphabet[key].probability = parseFloat((alphabet[key].frequency / realLength).toFixed(4));
-        alphabet[key].entropy = -parseFloat((Math.log(alphabet[key].probability)).toFixed(4));
+        alphabet[key].probability = alphabet[key].frequency / realLength;
+        alphabet[key].entropy = -Math.log(alphabet[key].probability);
     }
 }
 
@@ -59,9 +52,30 @@ function fillAdditionalInfo() {
  * Выписывает символ, его вероятность и энтропию
  */
 function printAlphabet() {
+    let punctuationMarksProbability = 0;
+    let keysArray = [];
+
+    for (let key in alphabet) {
+        keysArray.push(key);
+    }
+
+    keysArray.sort();
+
     console.log("s  probability     entropy");
-    for (var key in alphabet) {
-        console.log(key + ":    " + alphabet[key].probability + "        " + alphabet[key].entropy);
+
+    for (let i = 0; i < keysArray.length; i++) {
+        let key = keysArray[i];
+
+        if ([33, 34, 39, 40, 41, 44, 45, 46, 58, 59, 63, 96].indexOf(key.charCodeAt(0)) !== -1) {
+            punctuationMarksProbability += alphabet[key].probability;
+        } else {
+            console.log(key + ":    " + alphabet[key].probability.toFixed(4) + "        " + alphabet[key].entropy.toFixed(4));
+        }
+    }
+
+    if (punctuationMarksProbability !== 0) {
+        console.log(",:    " + punctuationMarksProbability.toFixed(4) + "        " +
+            -Math.log(punctuationMarksProbability).toFixed(4));
     }
 }
 
@@ -69,9 +83,9 @@ function printAlphabet() {
  * Расчитывает и выписывает энтропию файла
  */
 function printEntropy() {
-    var entropy = 0;
+    let entropy = 0;
 
-    for (var key in alphabet) {
+    for (let key in alphabet) {
         entropy += alphabet[key].probability * alphabet[key].entropy;
     }
 
@@ -82,43 +96,29 @@ function printEntropy() {
  * Расчитывает и выписывает энтропию файла при наличии статистических связей между двумя символами
  */
 function printLinkedEntropy() {
-    var entropy = 0;
+    let entropy = 0;
+    let alphabetSymbols = '';
 
-    for (var j in alphabet) {
-        for (var i in alphabet) {
-            const substituteJ = punctuationMarks.test(j) ? ':' : j;
-            const substituteI = punctuationMarks.test(i) ? ':' : i;
-            const allPairs = fileContent.match(new RegExp(j + '.', 'ig'));
-            var searchedPairs;
+    for (let key in alphabet) {
+        alphabetSymbols += key + '|';
+    }
 
-            if (punctuationMarks.test(j)) {
-                allPairs = fileContent.match(new RegExp(j + '.', 'ig'));
+    alphabetSymbols = alphabetSymbols.substr(0, alphabetSymbols.length - 1);
+
+    for (let j in alphabet) {
+        const allPairsCount = fileContent.match(new RegExp('(?=' + j + '(' + alphabetSymbols + '))', 'gi')) === null ? 0 :
+            fileContent.match(new RegExp('(?=' + j + '(' + alphabetSymbols + '))', 'gi')).length;
+
+        for (let i in alphabet) {
+            const searchedPairsCount = fileContent.match(new RegExp(j + i, 'ig')) === null ? 0 :
+                fileContent.match(new RegExp(j + i, 'ig')).length;
+            const probability = searchedPairsCount / allPairsCount;
+
+            if (probability !== 0) {
+                entropy += probability * alphabet[j].probability * -Math.log(probability);
             }
-            const searchedPairs = fileContent.match(new RegExp(j + i, 'ig'));
         }
     }
 
-    for (var i = 0; i < textLength; i++) {
-        var symbol = fileContent[i].toLowerCase();
-
-        if (/[^\t\v\r\n\f]/.test(symbol)) {
-            if (!/([a-z]|[0-9]| )/.test(symbol)) {
-                symbol = ',';
-            }
-
-            if (alphabet[symbol] === undefined) {
-                alphabet[symbol] = 1;
-            } else {
-                alphabet[symbol]++;
-            }
-
-            realLength++;
-        }
-    }
-
-    for (var key in alphabet) {
-        entropy += alphabet[key].probability * alphabet[key].entropy;
-    }
-
-    console.log("\nЭнтропия файла равна " + entropy.toFixed(4));
+    console.log("Энтропия файла при наличии односвязной цепи равна " + entropy.toFixed(4));
 }
